aca789d0ffa59015a2577d2d0a1cf699e66709ab
Gabrielbrrf <Gabrielfernandinho3210@gmail.com>
2025-06-09 11:02:52 -0300

    Corrige conexão com MySQL usando porta 3307

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@ $usuario = "root";
 $senha = "";
 $banco = "sistema_transporte";
 
-$conexao = new mysqli($host, $usuario, $senha, $banco);
+$conexao = new mysqli($host, $usuario, $senha, $banco, 3307);
 
 if ($conexao->connect_error) {
     die("Erro na conexão: " . $conexao->connect_error);
